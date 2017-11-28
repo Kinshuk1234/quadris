5b6f67b6e63812c8b372d9e4ca6d93fc9f3f67fe
Vinit Soni <vinitsoni9869@gmail.com>
2017-11-27 20:52:28 -0500

    add block

--- project/block.h
+++ project/block.h
@@ -2,7 +2,20 @@
 #define BLOCK_H
 
 class Block {
-	
+	// Orientations: 0 is North, 1 is East, 2 is South, 3 is West
+	// To get the clockwise rotation orientation, get (currOrientation + 1) % 4
+	// To get the counter-clockwise rotation orientation, get (currOrientation - 1) % 4
+	std::vector<std::vector<Pos>> orientations;
+	int currOrientation; // One of 0, 1, 2, 3
+public:
+
+	// Big 5 + ctor
+	Block() = default;
+	Block(const Block &other) = delete;
+	Block(Block &&other) = delete;
+	Block &operator=(const Block &other) = delete;
+	Block &operator=(Block &&other) = delete;
+	~Block() = default;
 };
 
 
--- project/lblock.h
+++ project/lblock.h
@@ -2,9 +2,16 @@
 #define L_BLOCK_H
 
 #include "block.h"
+#include <vector>
 
 class LBlock : public Block {
-
+public:
+	LBlock() = default;
+	LBlock(const LBlock &other) = delete;
+	LBlock(LBlock &&other) = delete;
+	LBlock &operator=(const LBlock &other) = delete;
+	LBlock &operator=(LBlock &&other) = delete;
+	~LBlock() = default;
 };
 
 
